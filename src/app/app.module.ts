53c838562ce514b093fd6d043aa83f29a5d9eaa0
go-tiger <dev.gotiger@gmail.com>
2025-10-21 11:12:47 +0900

    feat: config 전역 설정

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { ProjectModule } from 'src/modules/project/project.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
