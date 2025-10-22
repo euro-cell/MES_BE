f56534e5b1bb622ca3e1e4656fa6f2bde9fe45f8
go-tiger <dev.gotiger@gmail.com>
2025-10-22 12:19:50 +0900

    fix: CORS origin 전체 허용

--- src/common/configs/cors.config.ts
+++ src/common/configs/cors.config.ts
@@ -2,7 +2,8 @@ import { ConfigService } from '@nestjs/config';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const createCorsConfig = (configService: ConfigService): CorsOptions => ({
-  origin: configService.get<string>('FRONTEND_ORIGIN') || 'http://localhost:5173',
+  // origin: configService.get<string>('FRONTEND_ORIGIN') || 'http://localhost:5173',
+  origin: true,
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
   credentials: true,
   exposedHeaders: ['set-cookie'],
