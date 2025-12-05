7a9eb24da21fefe616e1196b641f615d04f0e390
go-tiger <dev.gotiger@gmail.com>
2025-12-05 17:53:56 +0900

    feat: 작업일지-바인더 기본 양식 파일 서빙 API

--- src/modules/production/worklog/worklog.controller.ts
+++ src/modules/production/worklog/worklog.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import type { Response } from 'express';
 import { WorklogService } from './worklog.service';
+import { join } from 'path';
 
 @Controller()
 export class WorklogController {
   constructor(private readonly worklogService: WorklogService) {}
+
+  @Get('binder')
+  async getBinderTemplate(@Res() res: Response) {
+    const dirPath = await this.worklogService.getTemplateFilePath();
+    const filePath = join(dirPath, 'binder.xlsx');
+    return res.download(filePath, 'binder.xlsx');
+  }
 }
--- src/modules/production/worklog/worklog.service.ts
+++ src/modules/production/worklog/worklog.service.ts
@@ -1,6 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class WorklogService {
-  constructor() {}
+  async getTemplateFilePath(): Promise<string> {
+    const filePath = join(process.cwd(), 'src', 'common', 'templates');
+
+    if (!existsSync(filePath)) {
+      throw new NotFoundException('템플릿 파일을 찾을 수 없습니다.');
+    }
+    return filePath;
+  }
 }
