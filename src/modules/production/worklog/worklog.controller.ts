82fc93c260c12568bfd9b7c26cb7f451647a8894
go-tiger <dev.gotiger@gmail.com>
2025-12-09 10:15:01 +0900

    feat: 작업일지-슬러리 기본 양식 파일 서빙 API

--- src/modules/production/worklog/worklog.controller.ts
+++ src/modules/production/worklog/worklog.controller.ts
@@ -13,4 +13,11 @@ export class WorklogController {
     const filePath = join(dirPath, '01.Binder.xlsx');
     return res.download(filePath, '01.Binder.xlsx');
   }
+
+  @Get('slurry')
+  async getSlurryTemplate(@Res() res: Response) {
+    const dirPath = await this.worklogService.getTemplateFilePath();
+    const filePath = join(dirPath, '02.Slurry.xlsx');
+    return res.download(filePath, '02.Slurry.xlsx');
+  }
 }
