e7c487b86ae5b142f930678c73ffe3d84eb0c450
go-tiger <dev.gotiger@gmail.com>
2025-12-09 17:21:11 +0900

    fix: 작업일지 기본 양식 파일 서빙 API

--- src/modules/production/worklog/worklog.controller.ts
+++ src/modules/production/worklog/worklog.controller.ts
@@ -1,32 +1,18 @@
 import { Controller, Get, Param, Res } from '@nestjs/common';
 import type { Response } from 'express';
-import { WorklogService } from './worklog.service';
-import { join } from 'path';
-
-const processTemplateFile = {
-  binder: '01.Binder.xlsx',
-  slurry: '02.Slurry.xlsx',
-  coating: '03.Coating.xlsx',
-  press: '04.Press.xlsx',
-  notching: '06.Notching.xlsx',
-  vd: '07.VD.xlsx',
-  forming: '08.Forming.xlsx',
-  stacking: '09.Stacking.xlsx',
-  welding: '10.Welding.xlsx',
-  sealing: '11.Sealing.xlsx',
-  formation: '13.Formation.xlsx',
-  grading: '14.Grading.xlsx',
-};
+import { ProcessTemplateFile, WorklogService } from './worklog.service';
+import { ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiParam } from '@nestjs/swagger';
 
 @Controller()
 export class WorklogController {
   constructor(private readonly worklogService: WorklogService) {}
 
   @Get(':process')
+  @ApiOperation({ summary: '공정별 작업일지 템플릿 파일 다운로드' })
+  @ApiParam({ name: 'process', enum: Object.keys(ProcessTemplateFile), description: '공정명' })
+  @ApiOkResponse({ description: '템플릿 파일 다운로드' })
+  @ApiNotFoundResponse({ description: '템플릿 파일을 찾을 수 없습니다.' })
   async getProcessTemplate(@Res() res: Response, @Param('process') process: string) {
-    const dirPath = await this.worklogService.getTemplateFilePath();
-    const fileName = processTemplateFile[process.toLowerCase()];
-    const filePath = join(dirPath, fileName);
-    return res.download(filePath, fileName);
+    return res.download(await this.worklogService.getTemplateFilePath(process));
   }
 }
--- src/modules/production/worklog/worklog.service.ts
+++ src/modules/production/worklog/worklog.service.ts
@@ -1,15 +1,33 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { existsSync } from 'fs';
 import { join } from 'path';
+
+export enum ProcessTemplateFile {
+  binder = '01.Binder.xlsx',
+  slurry = '02.Slurry.xlsx',
+  coating = '03.Coating.xlsx',
+  press = '04.Press.xlsx',
+  notching = '06.Notching.xlsx',
+  vd = '07.VD.xlsx',
+  forming = '08.Forming.xlsx',
+  stacking = '09.Stacking.xlsx',
+  welding = '10.Welding.xlsx',
+  sealing = '11.Sealing.xlsx',
+  formation = '13.Formation.xlsx',
+  grading = '14.Grading.xlsx',
+}
 
 @Injectable()
 export class WorklogService {
-  async getTemplateFilePath(): Promise<string> {
-    const filePath = join(process.cwd(), 'src', 'common', 'templates');
+  async getTemplateFilePath(processName: string): Promise<string> {
+    try {
+      const templatePath = join(process.cwd(), 'src', 'common', 'templates');
 
-    if (!existsSync(filePath)) {
+      const fileName = ProcessTemplateFile[processName.toLowerCase()];
+      const filePath = join(templatePath, fileName);
+
+      return filePath;
+    } catch (error) {
       throw new NotFoundException('템플릿 파일을 찾을 수 없습니다.');
     }
-    return filePath;
   }
 }
