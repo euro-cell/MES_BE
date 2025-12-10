cee5cca34bd97cc395792096881804cbd06c63af
go-tiger <dev.gotiger@gmail.com>
2025-12-10 12:15:49 +0900

    fix: 작업일지 공정별 모듈 배럴 패턴으로 변경

---
+++ src/modules/production/worklog/index.ts
@@ -0,0 +1,5 @@
+import { BinderModule } from './01-binder/binder.module';
+import { SlurryModule } from './02-slurry/slurry.module';
+import { CoatingModule } from './03-coating/coating.module';
+
+export { BinderModule, SlurryModule, CoatingModule };
--- src/modules/production/worklog/worklog.module.ts
+++ src/modules/production/worklog/worklog.module.ts
@@ -2,17 +2,18 @@ import { Module } from '@nestjs/common';
 import { RouterModule } from '@nestjs/core';
 import { WorklogService } from './worklog.service';
 import { WorklogController } from './worklog.controller';
-import { BinderModule } from './01-binder/binder.module';
-import { SlurryModule } from './02-slurry/slurry.module';
+import { BinderModule, SlurryModule, CoatingModule } from './index';
 
 @Module({
   imports: [
     RouterModule.register([
       { path: 'production', module: BinderModule },
       { path: 'production', module: SlurryModule },
+      { path: 'production', module: CoatingModule },
     ]),
     BinderModule,
     SlurryModule,
+    CoatingModule,
   ],
   controllers: [WorklogController],
   providers: [WorklogService],
