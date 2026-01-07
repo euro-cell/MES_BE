72a32dfb861d41440d43a3c5d835943f0f625352
go-tiger <dev.gotiger@gmail.com>
2026-01-07 09:16:58 +0900

    feat: 셀 관리 엔티티

---
+++ src/common/entities/cell-inventory.entity.ts
@@ -0,0 +1,66 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { CellGrade } from '../enums/cell-inventory.enum';
+import { Exclude } from 'class-transformer';
+
+@Entity('cell_inventories')
+export class CellInventory {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  lotNo: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  projectName: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  projectNo: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  model: string;
+
+  @Column({ type: 'enum', enum: CellGrade, default: CellGrade.GOOD })
+  grade: CellGrade;
+
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  ncrGrade: string | null;
+
+  @Column({ type: 'date' })
+  storageDate: Date;
+
+  @Column({ type: 'varchar', length: 100 })
+  storageLocation: string;
+
+  @Column({ type: 'date', nullable: true })
+  shippingDate: Date | null;
+
+  @Column({ type: 'varchar', length: 100, nullable: true })
+  shippingStatus: string | null;
+
+  @Column({ type: 'varchar', length: 50 })
+  deliverer: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  receiver: string;
+
+  @Column({ type: 'text', nullable: true })
+  details: string | null;
+
+  @Column({ type: 'boolean', default: false })
+  isShipped: boolean;
+
+  @Column({ type: 'boolean', default: false })
+  isRestocked: boolean;
+
+  @Exclude()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Exclude()
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Exclude()
+  @DeleteDateColumn()
+  deletedAt?: Date;
+}
---
+++ src/common/enums/cell-inventory.enum.ts
@@ -0,0 +1,5 @@
+export enum CellGrade {
+  GOOD = '양품',
+  NCR = 'ncr',
+  NG = 'ng',
+}
