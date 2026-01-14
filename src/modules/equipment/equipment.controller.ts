7736c7fbf76a043c94127d686d32d457fc0d6698
go-tiger <dev.gotiger@gmail.com>
2026-01-14 13:41:36 +0900

    feat: 설비 등록

--- src/modules/equipment/equipment.controller.ts
+++ src/modules/equipment/equipment.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { EquipmentService } from './equipment.service';
+import { CreateEquipmentDto } from 'src/common/dtos/equipment.dto';
 
 @Controller('equipment')
 export class EquipmentController {
   constructor(private readonly equipmentService: EquipmentService) {}
+
+  @Post()
+  async create(@Body() createEquipmentDto: CreateEquipmentDto) {
+    return this.equipmentService.create(createEquipmentDto);
+  }
 }
--- src/modules/equipment/equipment.service.ts
+++ src/modules/equipment/equipment.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Equipment } from 'src/common/entities/equipment.entity';
+import { CreateEquipmentDto } from 'src/common/dtos/equipment.dto';
 
 @Injectable()
-export class EquipmentService {}
+export class EquipmentService {
+  constructor(
+    @InjectRepository(Equipment)
+    private readonly equipmentRepository: Repository<Equipment>,
+  ) {}
+
+  async create(createEquipmentDto: CreateEquipmentDto): Promise<Equipment> {
+    const equipment = this.equipmentRepository.create(createEquipmentDto);
+    return this.equipmentRepository.save(equipment);
+  }
+}
