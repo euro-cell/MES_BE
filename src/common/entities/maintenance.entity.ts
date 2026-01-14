b833dd93bbe40b2220b14266c34bca6952be9d65
go-tiger <dev.gotiger@gmail.com>
2026-01-14 14:55:20 +0900

    feat: 설비-유지보수 엔티티

---
+++ src/common/entities/maintenance.entity.ts
@@ -0,0 +1,50 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+import { Equipment } from './equipment.entity';
+
+@Entity('maintenances')
+export class Maintenance {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  equipmentId: number;
+
+  @ManyToOne(() => Equipment, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'equipment_id' })
+  equipment: Equipment;
+
+  @Column({ type: 'date' })
+  inspectionDate: Date;
+
+  @Column({ length: 500 })
+  replacementHistory: string;
+
+  @Column({ length: 500 })
+  usedParts: string;
+
+  @Column({ length: 50 })
+  maintainer: string;
+
+  @Column({ length: 50 })
+  verifier: string;
+
+  @Column({ type: 'text', nullable: true })
+  remark: string;
+
+  @Exclude()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Exclude()
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
